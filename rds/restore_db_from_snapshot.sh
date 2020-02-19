b0068fd7c49f4614fdee99141e980a3bd0a11aff
Julio Silveira Melo <juliocsmelo@gmail.com>
2020-02-19 12:09:00 -0300

    chore: rds restore from snapshot script

---
+++ rds/restore_db_from_snapshot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+db_instance_identifier=$2
+db_snapshot_identifier=$3
+vpc_id=$4
+db_instance_class=$5
+
+restore_from_snapshot(){
+    echo "[$(date +%FT%T)] - Starting restoring the snapshot $db_snapshot_identifier"
+    echo "[$(date +%FT%T)] - Database indentifier $db_instance_identifier"
+    
+    aws rds restore-db-instance-from-db-snapshot \
+    --db-snapshot-identifier "$db_snapshot_identifier" \
+    --no-publicly-accessible --vpc-security-group-ids "$vpc_id" \
+    --deletion-protection --db-name "$db_name" \
+    --db-instance-identifier "$db_instance_identifier" \
+    --db-instance-class "$db_instance_class"
+    
+    echo "[$(date +%FT%T)] - Waiting database to be available $db_instance_identifier"
+    echo "[$(date +%FT%T)] - Database indentifier $db_instance_identifier"
+    
+    aws rds wait db-instance-available --db-instance-identifier "$db_instance_identifier"
+    echo "[$(date +%FT%T)] - Done $db_instance_identifier"
+}
+
+case "$1" in
+    --restore)
+        restore_from_snapshot
+    ;;
+    *)
+        echo "Usage: ./restore_db_from_snapshot.sh --restore <db_instance_identifier> <db_snapshot_identifier> <vpc_id> <db_instance_class>"
+    ;;
+esac
